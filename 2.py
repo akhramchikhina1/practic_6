660f94282105a12a81aa412bb6e6be13fedf05e4
akhramchikhina1 <a.khramchikhina1@g.nsu.ru>
2024-03-22 21:56:52 +0700

    print

--- 2.py
+++ 2.py
@@ -1,7 +1,7 @@
 a, b= map(int, input().split('x'))
 c, d, e= map(int, input().split('x'))
-if min(a,b) >= min(c,d,e):
-    if max(a,b) >= max(c,d,e):
+if min(a,b) >= min(c,d):
+    if max(a,b) >= max(c,d):
         print('да')
     else:
         print('нет')
