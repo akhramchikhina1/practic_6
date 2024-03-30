8897992f57f0df94c39783e1efa52e4e23706b47
akhramchikhina1 <159693504+akhramchikhina1@users.noreply.github.com>
2024-03-30 09:28:59 +0700

    5.py

--- 5.py
+++ 5.py
@@ -1,15 +1,7 @@
 counter_1, counter_2 = map(str,input().split('-'))
 x=counter_1[0]
 y=counter_2[0]
-if x == a:
-    a=str(1)
-b=str(2)
-c=str(3)
-d=4
-e=5
-f=6
-g=7
-h=8
+[a, b, c, d, e, f, g, h]= [1,2,3,4;5;6,7,8]
 if (abs(int(counter_1[0])-int(counter_2[0]))==2 and abs(int(counter_1[0])-int(counter_2[0]))==1) or (abs(int(counter_1[0])-int(counter_2[0]))==1 and abs(int(counter_1[0])-int(counter_2[0]))==2):
     if (abs(int(counter_1[1]) - int(counter_2[1])) == 2 or abs(int(counter_1[1]) - int(counter_2[1])) == 1) or (abs(int(counter_1[1]) - int(counter_2[1])) == 1 or abs(int(counter_1[1]) - int(counter_2[1])) == 2):
         print('верно')
