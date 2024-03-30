14ec5d7778168d3b53a0ea37de92f2a2cddeea24
akhramchikhina1 <159693504+akhramchikhina1@users.noreply.github.com>
2024-03-30 10:57:39 +0700

    5.py

--- 5.py
+++ 5.py
@@ -1,11 +1,6 @@
-counter_1, counter_2 = map(str,input().split('-'))
-x=counter_1[0]
-y=counter_2[0]
-dic={'a':1,'b':2,'c':3,'d':4,'e':5,'f':6,'g':7,'h':8}
-if (abs(int(counter_1[0])-int(counter_2[0]))==2 and abs(int(counter_1[0])-int(counter_2[0]))==1) or (abs(int(counter_1[0])-int(counter_2[0]))==1 and abs(int(counter_1[0])-int(counter_2[0]))==2):
-    if (abs(int(counter_1[1]) - int(counter_2[1])) == 2 or abs(int(counter_1[1]) - int(counter_2[1])) == 1) or (abs(int(counter_1[1]) - int(counter_2[1])) == 1 or abs(int(counter_1[1]) - int(counter_2[1])) == 2):
-        print('верно')
-    else:
-        print('ошибка')
-else:
-    print('ошибка')
+klet = input() 
+alphabet = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6, 'g': 7, 'h': 8} 
+if (abs(alphabet[klet[0]] - alphabet[klet[3]]) + abs(alphabet[klet[1]] - alphabet[klet[4]])) == 3: 
+    print('верно') 
+else: 
+    print('неверно')
